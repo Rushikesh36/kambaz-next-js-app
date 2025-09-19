caf6fb599d41700420bcbe9a87ca5c6376e601fc
Rushikesh Wani <rushikeshwani36@gmail.com>
2025-09-18 20:48:32 -0400

    Added some identification on Labs page

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -2,6 +2,11 @@ import Link from "next/link";
 export default function Labs() {
     return (
         <div id="wd-labs">
+            <div id="self-info">
+                <h2>Name: Rushikesh Rajendra Wani</h2>
+                <h3>Section: 04</h3>
+                <h3>Course Code: CS 5610</h3>
+            </div>
             <h1>Labs</h1>
             <ul>
                 <li>
